b46f0bb58d81d912f78d29fb82ec7a864c95a6ca
Vince Wong <mail.vince@gmail.com>
2018-11-22 21:30:17 +0000

    Multi-colour spots

--- prototypes/sampler.js
+++ prototypes/sampler.js
@@ -17,7 +17,7 @@ class Sampler {
 			return hand.containsPoint([x + dx, y + dy]);
 		});
 
-		// Return count of true values, as a ratio of all values.
-		return results.reduce((accumulator, currentValue) => currentValue ? ++accumulator : accumulator, 0) / this.samples.length;
+		// Return count of true values.
+		return results.reduce((accumulator, currentValue) => currentValue ? ++accumulator : accumulator, 0);
 	}
 }
